2c6a011b1a426b357df2b0a616513d9d0db6206b
Cytosine <1316264211@qq.com>
2017-01-01 00:54:54 +0800

    upload my first file,getBaiDuHomePage
    Thank @wizardforcel very much.With his directions,I can write it out.
    If you have any directions or advice about this or my English,please tell me,I will appreciate it.Thank you very much.
    (There should be a bow >,<)
    
    Besides,this file also have many wrong,like displaying Chinese as messy code.
    But now it is too late,Cytosine feels a bit tired,she wants to go sleep. QAQ
    
    Good night,funny GitHub>,<

---
+++ getBaiDuHomePage.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ * @author Cytosine
+ * Thank wizardforcel very much.With his directions,I can write it out.
+ * I will also appreciate it if you can give any directions or advice about it.Thank u.
+ * 
+ */
+
+import java.net.*;
+import java.io.*;
+
+public class getBaiDuHomePage {
+	public static void main(String[] args) {
+		StringBuffer result=new StringBuffer("");
+		BufferedReader bufferedReader=null;
+		try{
+			URL url=new URL("http://www.baidu.com");
+			URLConnection connection =url.openConnection();
+			connection.connect();
+			bufferedReader=new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+		try{
+			String line;
+			while((line = bufferedReader.readLine())!=null){
+				result.append(line);
+			}
+			bufferedReader.close();
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+		System.out.println(result);
+	}
+}
