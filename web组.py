9335b0396937335a41764cc27aa2b292e75a4ab9
Cytosine <Lovegood@users.noreply.github.com>
2017-10-20 19:36:41 +0800

    粗略分类和危害出来了

--- web组.py
+++ web组.py
@@ -1,11 +1,35 @@
 import xml.etree.ElementTree as ET
 from openpyxl import Workbook
 
+def category(str):
+    result = 'unknow'
+    if str.find('code execution') != -1:
+        result = '代码执行'
+    elif str.find('XSS') != -1:
+        result = 'XSS'
+    elif str.find('CSRF') != -1:
+        result = 'CSRF'
+    elif str.find('SQL injection') != -1:
+        result = 'SQL注入'
+    elif str.find('denial of service') != -1:
+        result = 'DOS'
+    return result
+
+def harm(str):
+    result = ''
+    if str.find('gain privileges') != -1:
+        result = '可提权 '
+    if str.find('cause a denial of service') != -1:
+        result += '造成DOS '
+    if str.find('information disclosure') != -1:
+        result += '造成信息泄露 '
+    return result
+
 def main():
     wb = Workbook() #create a workbook
     ws = wb.get_active_sheet() #调用正在运行的工作表 worksheet
     ws.title = 'raw'
-    rowCnt = 1
+    rowCnt = 2
 
     xmlPath = 'allitems-cvrf-year-2017.xml'
     xmlTagPrefix = '{http://www.icasi.org/CVRF/schema/vuln/1.1}'
@@ -16,8 +40,12 @@ def main():
             dataNode = child
             CVEtitle = dataNode.find(xmlTagPrefix+'Title').text
             description = dataNode.find(xmlTagPrefix+'Notes').find(xmlTagPrefix+'Note').text
+            if description.find('** RESERVED **') != -1:
+                continue;
             ws.cell(row = rowCnt, column = 1).value = CVEtitle
-            ws.cell(row = rowCnt, column = 2).value = description
+            ws.cell(row = rowCnt, column = 2).value = category(description)
+            ws.cell(row = rowCnt, column = 3).value = harm(description)
+            ws.cell(row = rowCnt, column = 4).value = description
             rowCnt += 1
 
     wb.save('raw.xlsx') # save
